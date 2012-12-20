6297f83a8b9de0176ae878d33b9a48886ef2f3de
Adam Bogdal <adam@bogdal.pl>
2012-12-20 08:45:08 +0100

    Include gcm urls

--- example/gcmserver/urls.py
+++ example/gcmserver/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
-    # gcm
-    url(r'^register/', 'gcm.views.device', name='register-device'),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('gcm.urls')),
 )
