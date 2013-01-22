cede37ab98ebfb802f6410a2a67442a13017facd
Dino Dwiyaksa <ddwiyaksha@gmail.com>
2013-01-22 07:57:25 +0700

    add unregister and return HttResponse 404 if failed to register/unregister

--- gcm/urls.py
+++ gcm/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gcm.views',
     url(r'^gcm/register/$', 'register', name='register-device'),
+    url(r'^gcm/unregister/$', 'unregister', name='unregister-device'),
 )
--- gcm/views.py
+++ gcm/views.py
@@ -1,8 +1,9 @@
 # -*- encoding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.simple import direct_to_template
+from django.shortcuts import get_object_or_404
 from gcm.models import Device
 
 @csrf_exempt
@@ -18,7 +19,20 @@ def register(request):
     if device_id is not None:
         device, created = Device.objects.get_or_create(dev_id=device_id)
         device.reg_id = request.POST.get('reg_id')
+        device.is_active = True
         device.save()
+        return HttpResponse()
 
-    return HttpResponse()
+    return HttpResponseNotFound()
 
+@csrf_exempt
+def unregister(request):
+    device_id = request.POST.get('device_id', None)
+    if device_id is not None:
+        device = get_object_or_404(Device, dev_id=device_id,)
+        if device.is_active:
+            device.is_active = False
+            device.save()
+            return HttpResponse()
+
+    return HttpResponseNotFound()
